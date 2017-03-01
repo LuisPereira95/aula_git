7b3f681425a4ff4fd129668b155f792ac67c667e
pedrcosta <pedro68535@gmail.com>
2017-03-01 15:27:50 +0000

    Add file adc.h

---
+++ adc.h
@@ -0,0 +1,30 @@
+#include "defines.h"
+
+using namespace std;
+
+class CAdc
+{
+	private:
+		int8_t mui_adcHandler;
+		int8_t mui_i2cAddress;
+		//int8_t mui_bitShift;
+
+		float mf_tmp36voltage;
+		float mf_temperature;
+		float mf_batteryLevel;
+
+		int16_t swap(int16_t value);
+
+	public:
+		CAdc();
+		~CAdc();
+
+		int16_t readADC_SingleEnded(int8_t channel);
+
+		void readTemperature();
+		float getTemperature();
+		float getTMP36Voltage();
+
+		void readBatteryLevel();
+		float getBatteryLevel();
+};
